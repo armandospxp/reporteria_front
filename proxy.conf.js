6fc281ecf64505967d5ba4966bc63d900b0aad33
Armando Rodriguez <arodriguez@negofin.com.py>
2023-04-28 09:30:50 -0400

    Correccón proxy.conf.js

---
+++ proxy.conf.js
@@ -0,0 +1,44 @@
+// {
+//     "/api": {
+//         "target": "http://127.0.0.1:8000",
+//         "secure": false,
+//         "pathRewrite": {
+//             "^/api": ""
+//         },
+//         "logLevel": "debug"
+//     }
+    
+// }
+
+
+const PROXY_CONFIG = [
+    {
+    context: [
+        "/api/reporteria",
+    ],
+    "target": "http://127.0.0.1:8000",
+    "secure": false,
+    "logLevel": "debug",
+    "changeOrigin": true,
+    "pathRewrite": {
+        "^/": ""
+    },
+    "bypass": function(proxyRes, req, res) {}
+    },
+    // {
+    //   context: [
+    //     "/oauth2/v1/",
+    //   ],
+    // // "target": "http://10.150.44.120:8080",
+    // "target": "https://jbdmzpre.personal.com.py",
+    //   "secure": true,
+    //   "logLevel": "debug",
+    //   "changeOrigin": true,
+    //   "pathRewrite": {
+    //     "^/": ""
+    //   },
+    //   // "bypass": function (proxyRes, req, res) {}
+    // }
+];
+
+module.exports = PROXY_CONFIG;
