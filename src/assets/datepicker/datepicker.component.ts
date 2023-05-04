6ee5dd88a27b74d64139f473b26934ba3c5ef816
Armando Rodriguez <arodriguez@negofin.com.py>
2023-05-04 11:54:04 -0400

    Datepicker language spanish

--- src/assets/datepicker/datepicker.component.ts
+++ src/assets/datepicker/datepicker.component.ts
@@ -1,15 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { CommonModule, JsonPipe } from '@angular/common';
-import { NgbAlertModule, NgbDateStruct, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAlertModule, NgbDateStruct, NgbDatepickerI18n, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+
+const I18N_VALUES: {[index: string]:any} = {
+	es: {
+		weekdays: ['Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa', 'Do'],
+		months: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+		weekLabel: 'sem',
+	},
+	// other languages you would support
+};
+
+// Define a service holding the language. You probably already have one if your app is i18ned. Or you could also
+// use the Angular LOCALE_ID value
+@Injectable()
+export class I18n {
+	language = 'es';
+}
+
+@Injectable()
+export class CustomDatepickerI18n extends NgbDatepickerI18n {
+	constructor(private _i18n: I18n) {
+		super();
+	}
+
+	getWeekdayLabel(weekday: number): string {
+		return I18N_VALUES[this._i18n.language].weekdays[weekday - 1];
+	}
+	override getWeekLabel(): string {
+		return I18N_VALUES[this._i18n.language].weekLabel;
+	}
+	getMonthShortName(month: number): string {
+		return I18N_VALUES[this._i18n.language].months[month - 1];
+	}
+	getMonthFullName(month: number): string {
+		return this.getMonthShortName(month);
+	}
+	getDayAriaLabel(date: NgbDateStruct): string {
+		return `${date.day}-${date.month}-${date.year}`;
+	}
+}
 
 @Component({
   selector: 'app-datepicker',
   standalone: true,
 	imports: [NgbDatepickerModule, NgbAlertModule, FormsModule, JsonPipe],
   templateUrl: './datepicker.component.html',
-  styleUrls: ['./datepicker.component.scss']
+  styleUrls: ['./datepicker.component.scss'],
+  providers: [I18n, { provide: NgbDatepickerI18n, useClass: CustomDatepickerI18n }], // define custom NgbDatepickerI18n provider
 })
-export class DatepickerComponent {
+export class DatepickerComponent  {
   model: NgbDateStruct | undefined;
 }
