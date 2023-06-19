cf393acff455471da6bf0c985fde6b8b00dcc2aa
Armando Rodriguez <arodriguez@negofin.com.py>
2023-06-19 12:47:27 -0400

    Valor de meta en meta vs actual

--- src/app/reportes/main/main.component.ts
+++ src/app/reportes/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Spinkit } from 'ng-http-loader';
 import { Banca } from './interfaces/bancas';
+import { ReportesService } from '../reportes.service';
 
 @Component({
   selector: 'app-main',
@@ -15,10 +16,18 @@ export class MainComponent {
   public spinkit = Spinkit;
   public date = new Date()
   public anio_actual = this.date.getFullYear();
+  meta:any;
 
   labels: Banca = { micro: { nombre: "Micro", valor: 130 }, consumo: {
     nombre: "Consumo", valor: 240 }, debito: { nombre: "Débito", valor: 250 } }
 
   ngOnInit() {
+  }
+
+  constructor(private reporteService: ReportesService){
+    this.reporteService.obtenerMetasFranquicias().subscribe((resp:any)=>{
+      console.log(resp);
+      this.meta = resp[0].value;
+    })
   }
 }
